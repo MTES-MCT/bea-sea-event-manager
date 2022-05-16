56b2dc445aa0238dd5eeb592f4c9a892d0b9a02d
Philippe GALVAN <git.philippeG@outlook.com>
2022-05-16 10:17:58 +0200

    feat: sitrep list

--- entry_helper/urls.py
+++ entry_helper/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('reports/', views.ReportListView.as_view()),
 ]
--- entry_helper/views.py
+++ entry_helper/views.py
@@ -9,3 +9,12 @@ def index(request):
     ]
     context = {'reports': reports, 'button_data': {'label': 'Label of the button item', 'onclick': "alert('button doing stuff')"}}
     return render(request, 'entry_helper/index.html', context)
+
+
+from django.views.generic import ListView
+
+from entry_helper.models import Report
+
+
+class ReportListView(ListView):
+    model = Report
