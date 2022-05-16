04e9f08053a099c913c9d2c15e4c47473e0b08eb
Philippe GALVAN <git.philippeG@outlook.com>
2022-05-16 17:33:05 +0200

    feat: update data model for report

--- sea_events_manager/entry_helper/models.py
+++ sea_events_manager/entry_helper/models.py
@@ -6,13 +6,18 @@ from django.db import models
 class Report(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     report_number = models.TextField()
-    ship_name = models.TextField()
+    ship_name = models.TextField(null=True)
     imo_number = models.TextField(null=True)
     registration_number = models.TextField(null=True)
     declarative_entity = models.TextField()
     event_location = models.TextField()
     event_datetime = models.DateTimeField()
     event_type = models.TextField()
+    ship_total_length = models.FloatField(null=True)
+    ship_type = models.TextField(null=True)
+    nb_deceased = models.IntegerField()
+    nb_lost = models.IntegerField()
+    nb_injured = models.IntegerField()
     status = models.TextField(default="todo")
 
     def __str__(self):
