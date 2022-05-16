93b44a421e740aac3ad29792666ed41941f07cf9
Philippe GALVAN <git.philippeG@outlook.com>
2022-05-16 16:28:38 +0200

    feat: remove old index view

--- sea_events_manager/entry_helper/urls.py
+++ sea_events_manager/entry_helper/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "entry_helper"
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("reports/done/", views.ReportDoneListView.as_view(), name="reports_done"),
+    path("", views.ReportTodoListView.as_view(), name="reports"),
     path("reports/", views.ReportTodoListView.as_view(), name="reports"),
+    path("reports/done/", views.ReportDoneListView.as_view(), name="reports_done"),
 ]
--- sea_events_manager/entry_helper/views.py
+++ sea_events_manager/entry_helper/views.py
@@ -1,22 +1,4 @@
-from django.shortcuts import render, redirect
-
-
-def index(request):
-    reports = [
-        "Report 1",
-        "Report 2",
-        "Report 3",
-    ]
-    context = {
-        "reports": reports,
-        "button_data": {
-            "label": "Label of the button item",
-            "onclick": "alert('button doing stuff')",
-        },
-    }
-    return render(request, "entry_helper/index.html", context)
-
-
+from django.shortcuts import redirect
 from django.views.generic import ListView
 
 from entry_helper.models import Report
