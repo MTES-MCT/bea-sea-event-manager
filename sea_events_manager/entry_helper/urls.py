7a2bfe6f68afb779849e445b43ce2d53fafc7ac1
Philippe GALVAN <git.philippeG@outlook.com>
2022-05-16 15:56:26 +0200

    feat: add option to push report in emcip

--- sea_events_manager/entry_helper/urls.py
+++ sea_events_manager/entry_helper/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "entry_helper"
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("reports/", views.ReportListView.as_view()),
+    path("reports/", views.ReportListView.as_view(), name="reports"),
 ]
--- sea_events_manager/entry_helper/views.py
+++ sea_events_manager/entry_helper/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def index(request):
@@ -20,9 +20,18 @@ def index(request):
 from django.views.generic import ListView
 
 from entry_helper.models import Report
+from entry_helper.internals import switch_report_to_done
 
 
 class ReportListView(ListView):
     model = Report
+    queryset = Report.objects.filter(status="todo")
 
     ordering = ["-event_datetime"]
+
+    def post(self, request):
+        if report_uuid := request.POST.get("report_uuid", None):
+            report = Report.objects.get(uuid=report_uuid)
+            switch_report_to_done(report)
+
+        return redirect("entry_helper:reports")
