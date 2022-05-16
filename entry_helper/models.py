5dd0d89ab64f926145e6729f18a0d71661b9b248
Philippe GALVAN <git.philippeG@outlook.com>
2022-05-16 09:57:23 +0200

    feat: add report model

--- entry_helper/models.py
+++ entry_helper/models.py
@@ -1,3 +1,18 @@
+from uuid import uuid4
+
 from django.db import models
 
-# Create your models here.
+
+class Report(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    report_number = models.TextField()
+    name = models.TextField()
+    imo_number = models.TextField(null=True)
+    national_id = models.TextField(null=True)
+    declarative_entity = models.TextField()
+    event_location = models.TextField()
+    event_datetime = models.DateTimeField()
+    event_type = models.TextField()
+
+    def __str__(self):
+        return self.report_number
