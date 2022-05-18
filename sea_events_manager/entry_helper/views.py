9056db6b187d473955040b8127f0d8a56ec57c8b
PhilippeGalvan <git.philippeG@outlook.com>
2022-05-18 17:54:11 +0200

    fix: report handling button broken and launching wrong actions

--- sea_events_manager/entry_helper/views.py
+++ sea_events_manager/entry_helper/views.py
@@ -3,7 +3,11 @@ from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from entry_helper.models import Report
-from entry_helper.internals import switch_report_to_done
+from entry_helper.internals import (
+    switch_report_to_done,
+    switch_report_to_ignored,
+    switch_report_to_todo,
+)
 
 from data_scripts.setup_bea_fake_data import task_load_fake_bea_data_into_django
 
@@ -29,7 +33,14 @@ class ReportTodoListView(ReportListView):
         return super().get(request)
 
     def post(self, request):
-        _post_with_push_to_emcip(request, "entry_helper:reports")
+        current_view = "entry_helper:reports"
+        match report_target_status := request.POST.get("report_target_status", None):
+            case "done":
+                return _post_with_push_to_emcip(request, current_view)
+            case "ignored":
+                return _post_with_ignored(request, current_view)
+            case _:
+                raise Exception(f"Unknown target status {report_target_status}")
 
 
 class ReportDoneListView(ReportListView):
@@ -52,12 +63,35 @@ class ReportIgnoredListView(ReportTodoListView):
     }
 
     def post(self, request):
-        _post_with_push_to_emcip(request, django_redirect_url="entry_helper:reports_ignored")
+        current_view = "entry_helper:reports_ignored"
+        match report_target_status := request.POST.get("report_target_status", None):
+            case "done":
+                return _post_with_push_to_emcip(request, current_view)
+            case "todo":
+                return _post_with_todo(request, current_view)
+            case _:
+                raise Exception(f"Unknown target status {report_target_status}")
 
 
 def _post_with_push_to_emcip(request, django_redirect_url: str):
     if report_uuid := request.POST.get("report_uuid", None):
         report = Report.objects.get(uuid=report_uuid)
         switch_report_to_done(report)
+
+    return redirect(django_redirect_url)
+
+
+def _post_with_ignored(request, django_redirect_url: str):
+    if report_uuid := request.POST.get("report_uuid", None):
+        report = Report.objects.get(uuid=report_uuid)
+        switch_report_to_ignored(report)
+
+    return redirect(django_redirect_url)
+
+
+def _post_with_todo(request, django_redirect_url: str):
+    if report_uuid := request.POST.get("report_uuid", None):
+        report = Report.objects.get(uuid=report_uuid)
+        switch_report_to_todo(report)
 
     return redirect(django_redirect_url)
