90930116c4bda6d8cdcb3a6a05cf2953f611d08b
Philippe GALVAN <git.philippeG@outlook.com>
2022-06-07 18:14:22 +0200

    fix: Report state management with tests

--- src/entry_helper/core.py
+++ src/entry_helper/core.py
@@ -1,9 +1,13 @@
 from entry_helper.emcip_service import BEAToEmcipService
 from entry_helper.models import Report
 
+def _prevent_edit_for_done_reports(report: Report) -> None:
+    if report.status == "done":
+        raise Exception("Report is already done")
 
 def switch_report_to_done(report: Report) -> None:
-    try: 
+    _prevent_edit_for_done_reports(report)
+    try:
         BEAToEmcipService().push_report_to_emcip(report)
     except Exception as e:
         print(e)
@@ -14,10 +18,12 @@ def switch_report_to_done(report: Report) -> None:
 
 
 def switch_report_to_ignored(report: Report) -> None:
+    _prevent_edit_for_done_reports(report)
     report.status = "ignored"
     report.save()
 
 
 def switch_report_to_todo(report: Report) -> None:
+    _prevent_edit_for_done_reports(report)
     report.status = "todo"
     report.save()
