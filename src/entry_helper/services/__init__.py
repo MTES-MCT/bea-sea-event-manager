8548010765620b9c917711772311ac7553302a19
Philippe GALVAN <git.philippeG@outlook.com>
2022-07-01 19:38:52 +0200

    refactor: add documentation to services as docstrings

--- src/entry_helper/services/__init__.py
+++ src/entry_helper/services/__init__.py
@@ -1,1 +1,6 @@
+"""
+Services are used as defined in Domain Driven Design terminology.
 
+They encapsulate complex logic unrelated to business rules intead this logic is
+related to external or delegated logic.
+"""
